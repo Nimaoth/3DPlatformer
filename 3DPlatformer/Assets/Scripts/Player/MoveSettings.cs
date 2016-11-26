bab4d13dc644ec03c313b6d178d2c6a93767ba97
Nimaoth <nicolas.otremba@web.de>
2016-11-26 19:45:07 +0100

    added player movement

---
+++ 3DPlatformer/Assets/Scripts/Player/MoveSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+[System.Serializable]
+public class MoveSettings
+{
+    public float RunVelocity = 12;
+    public float RotateVelocity = 100;
+    public float JumpVelocity = 8;
+    public float DistanceToGround = 1.3f;
+    public LayerMask Ground;
+}
--- 3DPlatformer/Assets/Scripts/Player/PlayerBehaviour.cs
+++ 3DPlatformer/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,15 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerBehaviour : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    #region Public Variables
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public MoveSettings MoveSettings;
+    public InputSettings InputSettings;
+
+    #endregion
+
+    #region Private Variables
+
+    private Rigidbody playerRigidbody;
+    private Vector3 velocity;
+    private Quaternion targetRotation;
+    private float forwardInput;
+    private float sidewaysInput;
+    private float turnInput;
+    private int jumpInput;
+
+    #endregion
+
+    void Awake()
+    {
+        velocity = Vector3.zero;
+        forwardInput = sidewaysInput = turnInput = jumpInput = 0;
+        targetRotation = transform.rotation;
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+
+    void GetInput()
+    {
+        if (InputSettings.FORWARD_AXIS.Length != 0)
+            forwardInput = Input.GetAxis(InputSettings.FORWARD_AXIS);
+
+        if (InputSettings.SIDEWAYS_AXIS.Length != 0)
+            sidewaysInput = Input.GetAxis(InputSettings.SIDEWAYS_AXIS);
+
+        if (InputSettings.TURN_AXIS.Length != 0)
+            turnInput = Input.GetAxis(InputSettings.TURN_AXIS);
+
+        if (InputSettings.JUMP_AXIS.Length != 0)
+            jumpInput = (int) Input.GetAxisRaw(InputSettings.JUMP_AXIS);
+    }
+
+    void Run()
+    {
+        velocity.z = forwardInput * MoveSettings.RunVelocity;
+        velocity.x = sidewaysInput * MoveSettings.RunVelocity;
+        velocity.y = playerRigidbody.velocity.y;
+        playerRigidbody.velocity = transform.TransformDirection(velocity);
+    }
+
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > 0)
+        {
+            targetRotation *= Quaternion.AngleAxis(MoveSettings.RotateVelocity * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+
+    void Jump()
+    {
+        if (jumpInput != 0 && Grounded())
+        {
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, MoveSettings.JumpVelocity,
+                playerRigidbody.velocity.z);
+        }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, MoveSettings.DistanceToGround, MoveSettings.Ground);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -MoveSettings.DistanceToGround, 0));
+    }
 }
